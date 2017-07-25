91b96dc6bed99f5c24948050d31df5e45ad081b7
Daniel Petti <djpetti@gmail.com>
2017-07-25 13:58:00 -0400

    Rewrite matlab image extractor.
    This time, it extracts the raw images, which can be used
    transparently with the rest of the data-processing pipeline.

--- matlab_to_images.py
+++ matlab_to_images.py
@@ -1,72 +1,139 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
+import argparse
 import os
-import sys
-
-import cv2
+import shutil
 
 import numpy as np
 import scipy.io
 
 
-""" Helper script that converts a Matlab dataset to an image sequence. """
-
-
-# Our screen size.
-SCREEN_WIDTH=1920
-SCREEN_HEIGHT=1080
-# Original screen size.
-KANG_SCREEN_WIDTH=2880
-KANG_SCREEN_HEIGHT=1620
-
-
-def load_mat_data(filename):
-  """ Loads and curates data from a matlab file.
+def load_gaze_data(filename):
+  """ Loads and curates gaze data from a matlab file.
   Args:
     filename: The file to load from.
   Returns:
-    The raw image data, in a Numpy array, and the corresponding label data. """
+    The raw gaze data, in a Numpy array. """
   mat_file = scipy.io.loadmat(filename)
-  return (mat_file["eye_left"], mat_file["gaze"])
+  return (mat_file["gaze"])
 
-def convert_label(label, label_index):
-  """ Converts a raw label to a form that we can easily parse with existing
-  code.
+def process_session(session_path, subject, session, all_image_data):
+  """ Processes data from a single session.
   Args:
-  label: The raw label.
-    label_index: A unique number for each label.
-  Returns:
-    The label as a string, for use with Myelin. """
-  # Convert screen sizes.
-  x_pos = label[0]
-  y_pos = label[1]
-  x_pos = x_pos / KANG_SCREEN_WIDTH * SCREEN_WIDTH
-  y_pos = y_pos / KANG_SCREEN_HEIGHT * SCREEN_HEIGHT
+    session_path: The path to a particular head pose group in that session.
+    subject: The name of the subject.
+    session: The name of the session.
+    all_image_data: List to write the processed data to. """
+  data_path = os.path.join(session_path, "raw_image")
+  if not os.path.exists(data_path):
+    print "Could not find data dir: %s" % (data_path)
+    return
 
-  return "%sx%s_%d.jpg" % (str(x_pos), str(y_pos), label_index)
+  # Compile file names for images.
+  image_numbers = []
+  for image_name in os.listdir(data_path):
+    if not image_name.endswith(".jpg"):
+      # Extraneous.
+      continue
+    image_numbers.append(int(image_name.rstrip(".jpg")))
+
+  # We need to load them in the right order to get the right gaze points.
+  image_numbers.sort()
+
+  # Include the full paths.
+  image_paths = []
+  for image_num in image_numbers:
+    image_paths.append(os.path.join(data_path, "%05d.jpg" % (image_num)))
+
+  # Path to the gaze data.
+  gaze_path = os.path.join(data_path, "gaze.mat")
+  # Load the relevant label data.
+  labels = load_gaze_data(gaze_path)
+
+  # Write everything into the main list.
+  for i in range(0, len(image_paths)):
+    image_path = image_paths[i]
+    label = labels[i]
+    all_image_data.append((image_path, label, subject, session))
+
+def load_all_subjects(dataset_dir):
+  """ Loads the data for all subjects.
+  Args:
+    dataset_dir: The base dataset directory.
+  Returns:
+    A list of tuples containing each image path, the label, the subject, and the
+    session. """
+  all_image_data = []
+
+  for subject in os.listdir(dataset_dir):
+    print "Loading data for %s..." % (subject)
+
+    subject_path = os.path.join(dataset_dir, subject)
+
+    if not os.path.isdir(subject_path):
+      # Something extraneous.
+      print "Ignoring extraneous subject: %s" % (subject)
+      continue
+
+    for day in os.listdir(subject_path):
+      day_path = os.path.join(subject_path, day)
+
+      if (not os.path.isdir(day_path) or not day.startswith("day")):
+        print "Ignoring extraneous day: %s" % (day)
+        continue
+
+      # Look at the head positions.
+      for head_pos in ["Left", "Center", "Right"]:
+        head_pos_path = os.path.join(day_path, head_pos)
+
+        process_session(head_pos_path, subject, day, all_image_data)
+
+  return all_image_data
+
+def copy_images(image_paths, out_dir, flatten=False):
+  """ Copies the images with the correct labeled names.
+  Args:
+    image_data: The image data returned by load_all_subjects.
+    out_dir: The base output directory.
+    flatten: If true, flatten output directory structure. """
+  for path, label, subject, session in image_paths:
+    session_path = None
+    if flatten:
+      session_path = os.path.join(out_dir, "%s_%s" % (subject, session))
+
+    else:
+      subject_path = os.path.join(out_dir, subject)
+      if not os.path.exists(subject_path):
+        print "Adding subject: %s" % (subject)
+        os.mkdir(subject_path)
+
+      session_path = os.path.join(subject_path, session)
+
+    if not os.path.exists(session_path):
+      print "Adding session: %s" % (session)
+      os.mkdir(session_path)
+
+    # Copy the proper images.
+    out_name = "%dx%d_-1_-1.jpg" % (label[0], label[1])
+    out_path = os.path.join(session_path, out_name)
+    shutil.copy(path, out_path)
 
 
 def main():
-  in_file = sys.argv[1]
-  out_dir = sys.argv[2]
+  # Parse user arguments.
+  parser = argparse.ArgumentParser( \
+      description="Convert a matlab dataset to raw images.")
+  parser.add_argument("dataset", help="The location of the dataset to convert.")
+  parser.add_argument("output", help="Location to write output to.")
+  parser.add_argument("-f", "--flatten", action="store_true",
+                      help="Flatten output directory structure.")
+  args = parser.parse_args()
 
-  # Load the Matlab data.
-  images, labels = load_mat_data(in_file)
-
-  # Write the output.
-  for i in range(0, images.shape[0]):
-    image = images[i]
-    label = labels[i]
-
-    # The image comes flipped.
-    image = np.fliplr(image)
-    image = np.flipud(image)
-
-    file_name = convert_label(label, i)
-    file_name = os.path.join(out_dir, file_name)
-
-    cv2.imwrite(file_name, image)
-
+  print "Analyzing dataset..."
+  image_data = load_all_subjects(args.dataset)
+  print "Copying dataset..."
+  copy_images(image_data, args.output, flatten=args.flatten)
+  print "Done!"
 
 if __name__ == "__main__":
   main()
